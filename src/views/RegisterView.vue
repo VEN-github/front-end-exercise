f5461f14fb32e3daf94109e5b4de52cfe46d8597
Raven Barrogo <barrogoraven@gmail.com>
2024-07-26 10:26:56 +0800

    ADD: register :sparkles:

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -7,31 +7,45 @@
           <CardDescription> Enter your information to create an account </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form @submit.prevent="onSubmit">
             <div class="grid gap-4">
               <div class="grid grid-cols-2 gap-4">
                 <div class="grid gap-2">
                   <Label for="first-name">First name</Label>
-                  <Input id="first-name" placeholder="John" required />
+                  <Input v-model="models.firstName" id="first-name" placeholder="John" required />
                 </div>
                 <div class="grid gap-2">
                   <Label for="last-name">Last name</Label>
-                  <Input id="last-name" placeholder="Doe" required />
+                  <Input v-model="models.lastName" id="last-name" placeholder="Doe" required />
                 </div>
               </div>
               <div class="grid gap-2">
                 <Label for="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
+                <Input
+                  v-model="models.email"
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
               </div>
               <div class="grid gap-2">
                 <Label for="password">Password</Label>
-                <Input id="password" type="password" password />
+                <Input v-model="models.password" id="password" type="password" password />
               </div>
               <div class="grid gap-2">
                 <Label for="confirm-password">Confirm Password</Label>
-                <Input id="confirm-password" type="password" password />
+                <Input
+                  v-model="models.confirmPassword"
+                  id="confirm-password"
+                  type="password"
+                  password
+                />
               </div>
-              <Button type="submit" class="w-full"> Create an account </Button>
+              <Button type="submit" class="w-full" :disabled="isSubmitting">
+                <LoaderCircle v-if="isSubmitting" class="w-5 h-5 animate-spin mr-2" />
+                {{ isSubmitting ? 'Please wait...' : 'Create an account' }}
+              </Button>
             </div>
           </form>
           <div class="mt-4 text-center text-sm">
@@ -45,11 +59,61 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+import { ref, reactive } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+import { useToast } from '@/components/ui/toast/use-toast';
+import { useFirebaseAuth } from 'vuefire';
+import { createUserWithEmailAndPassword, updateProfile } from '@firebase/auth';
+import { LoaderCircle } from 'lucide-vue-next';
 
 import BaseContainer from '@/components/ui/base/BaseContainer.vue';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+
+const auth = useFirebaseAuth();
+const router = useRouter();
+const models = reactive({
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+});
+const { toast } = useToast();
+const isSubmitting = ref<boolean>(false);
+
+async function onSubmit() {
+  if (models.password !== models.confirmPassword) {
+    toast({
+      title: 'Error',
+      variant: 'destructive',
+      description: 'Password does not match.',
+    });
+    return;
+  }
+
+  isSubmitting.value = true;
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      models.email,
+      models.password
+    );
+    const user = userCredential.user;
+    updateProfile(user, {
+      displayName: `${models.firstName} ${models.lastName}`,
+    });
+    router.push('/');
+  } catch (error) {
+    toast({
+      title: 'Error',
+      variant: 'destructive',
+      description: 'Something went wrong. Please try again.',
+    });
+  } finally {
+    isSubmitting.value = false;
+  }
+}
 </script>
